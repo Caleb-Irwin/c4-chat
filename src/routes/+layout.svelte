e088d6c3826007c9ec0f63ecc535ed94ad2d0e76
Caleb Irwin <me@calebirwin.ca>
2025-06-07 23:39:27 -0400

    add convex!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { setupConvex } from 'convex-svelte';
 	import '../app.css';
 
 	let { children } = $props();
+
+	setupConvex(PUBLIC_CONVEX_URL);
 </script>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,26 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
+	import { useQuery } from 'convex-svelte';
+	import { api } from '../convex/_generated/api';
+
+	const query = useQuery(
+		api.messages.helloWorld,
+		{},
+		{
+			keepPreviousData: true
+		}
+	);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <Button>Hello World!</Button>
+<p>
+	{#if query.isLoading}
+		Loading...
+	{:else if query.error}
+		Error: {query.error.message}
+	{:else}
+		{query.data}
+	{/if}
+</p>
